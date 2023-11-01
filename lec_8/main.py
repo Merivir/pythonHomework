749854745e32d1416c8bb2ca4760bf8db2a2e4f3
Merivir <118295002+Merivir@users.noreply.github.com>
2023-11-01 14:33:56 +0400

    Update main.py

--- lec_8/main.py
+++ lec_8/main.py
@@ -1,52 +1,52 @@
-class Matrix:
-    def __init__(self, n, m):
-        self.n = n
-        self.m = m
-        self.matrix = [[0] * m for _ in range(n)]
+class CustomMatrix:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.data = [[0] * cols for _ in range(rows)]
 
-    def print_matrix(self):
-        for row in self.matrix:
+    def display(self):
+        for row in self.data:
             print(row)
 
     def calculate_mean(self):
-        total = sum(sum(row) for row in self.matrix)
-        return total / (self.n * self.m)
+        total = sum(sum(row) for row in self.data)
+        return total / (self.rows * self.cols)
 
-    def calculate_row_sum(self, row_index):
-        if 0 <= row_index < self.n:
-            return sum(self.matrix[row_index])
+    def calculate_row_sum(self, row_idx):
+        if 0 <= row_idx < self.rows:
+            return sum(self.data[row_idx])
         else:
             return None
 
-    def calculate_column_average(self, column_index):
-        if 0 <= column_index < self.m:
-            total = sum(row[column_index] for row in self.matrix)
-            return total / self.n
+    def calculate_column_average(self, col_idx):
+        if 0 <= col_idx < self.cols:
+            total = sum(row[col_idx] for row in self.data)
+            return total / self.rows
         else:
             return None
 
-    def print_submatrix(self, indices):
-        col1, col2, row1, row2 = indices
-        if 0 <= col1 < col2 <= self.m and 0 <= row1 < row2 <= self.n:
-            submatrix = [row[col1:col2] for row in self.matrix[row1:row2]]
+    def display_submatrix(self, indices):
+        col_start, col_end, row_start, row_end = indices
+        if 0 <= col_start < col_end <= self.cols and 0 <= row_start < row_end <= self.rows:
+            submatrix = [row[col_start:col_end] for row in self.data[row_start:row_end]]
             for row in submatrix:
                 print(row)
         else:
             print("Invalid submatrix indices.")
 
-n = 4
-m = 3
-matrix = Matrix(n, m)
+rows = 4
+cols = 3
+matrix = CustomMatrix(rows, cols)
 
-matrix.matrix = [
+matrix.data = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
     [10, 11, 12]
 ]
 
-print("Matrix:")
-matrix.print_matrix()
+print("Custom Matrix:")
+matrix.display()
 
 mean = matrix.calculate_mean()
 print(f"Mean of the matrix: {mean}")
@@ -63,5 +63,5 @@ if col_average is not None:
 else:
     print("Invalid column index.")
 
-print("Submatrix:")
-matrix.print_submatrix([1, 3, 0, 2])  
+print("Custom Submatrix:")
+matrix.display_submatrix([1, 3, 0, 2])
